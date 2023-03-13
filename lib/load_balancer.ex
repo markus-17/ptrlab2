9fac3712560be0dfd82763129ec181f45c5a84d1
markus-17 <marius.purici@isa.utm.md>
2023-03-13 21:07:58 +0200

    Week2 Bonus Task

--- lib/load_balancer.ex
+++ lib/load_balancer.ex
@@ -8,13 +8,22 @@ defmodule LoadBalancer do
 
   @impl true
   def init(nr_of_printers) do
-    {:ok, {0, nr_of_printers}}
+    {:ok, {nr_of_printers}}
   end
 
   @impl true
-  def handle_info(message, {current_printer, nr_of_printers}) do
+  def handle_info("GO KILL YOURSELF", {nr_of_printers}) do
+    printer_name = :"printer#{Enum.random(1..nr_of_printers)}"
+    if Process.whereis(printer_name) != nil, do: send(printer_name, "GO KILL YOURSELF")
+    {:noreply, {nr_of_printers}}
+  end
+
+  @impl true
+  def handle_info(message, {nr_of_printers}) do
+    hash_distribution_key = message["message"]["tweet"]["text"]
+    current_printer = :crypto.hash(:sha256, hash_distribution_key) |> :binary.last() |> rem(3)
     printer_name = :"printer#{current_printer + 1}"
     if Process.whereis(printer_name) != nil, do: send(printer_name, message)
-    {:noreply, {rem(current_printer + 1, nr_of_printers), nr_of_printers}}
+    {:noreply, {nr_of_printers}}
   end
 end
